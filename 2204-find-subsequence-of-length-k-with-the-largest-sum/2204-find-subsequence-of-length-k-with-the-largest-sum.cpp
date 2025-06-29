99611230ddb257902d8238c04dbcda29123c0ec3
Harsh Yadav <harsh153658@gmail.com>
2025-06-30 00:34:39 +0530

    Time: 3 ms (43.37%), Space: 16.6 MB (5.55%) - LeetHub

--- 2204-find-subsequence-of-length-k-with-the-largest-sum/2204-find-subsequence-of-length-k-with-the-largest-sum.cpp
+++ 2204-find-subsequence-of-length-k-with-the-largest-sum/2204-find-subsequence-of-length-k-with-the-largest-sum.cpp
@@ -1,27 +1,26 @@
+#define pb push_back
+#define F first
+#define S second
 class Solution {
 public:
-    vector<int> maxSubsequence(vector<int>& nums, int k) {
-        
-        vector<pair<int,int>>v;
-
-        for(int i = 0; i<nums.size(); i++){
-            v.push_back(make_pair(nums[i],i));
+    vector<int> maxSubsequence(vector<int>&arr, int k) {
+        int n=arr.size();
+        vector<pair<int,int>>vp(n);
+        for(int i=0;i<n;i++){
+            vp[i]={arr[i],i};
         }
-        sort(v.begin(),v.end(), [](const pair<int, int>& a, const pair<int,int>& b) {
-            return a.first > b.first;
-        });
-
-        vector<pair<int,int>> topK(v.begin(),v.begin() + k);
-
-        sort(topK.begin(),topK.end(), [](const pair<int,int> & a, const pair<int, int> & b){
-            return a.second < b.second;
-        });
-
-        vector<int>result;
-        for(int i = 0; i< topK.size(); i++){
-            result.push_back(topK[i].first);
+        sort(vp.rbegin(),vp.rend());
+        vector<pair<int,int>>ans(k);
+        for(int i=0;i<k;i++){
+            ans[i]=vp[i];
         }
-
-        return result;
+        sort(ans.begin(),ans.end(),[&](pair<int,int>&a,pair<int,int>&b){
+            return a.S<b.S;
+        });
+        vector<int>res(k);
+        for(int i=0;i<k;i++){
+            res[i]=ans[i].F;
+        }
+        return res;
     }
 };
